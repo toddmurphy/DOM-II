8562c8a6e34b794d61c295a7317160d4cd163530
toddmurphy <toddmurphy93@gmail.com>
2019-10-29 19:26:27 -0300

    updated event listeners

--- js/index.js
+++ js/index.js
@@ -5,54 +5,54 @@ busHeader.addEventListener('mouseover', () => {
   busHeader.style.color = 'red';
 });
 
-//keydown event (not sure how to get this working)
-const imgKeyDown = document.querySelector('.content-section .img-content .img');
-imgKeyDown.addEventListener('keydown', () => {
-  event.code === 'KeyD';
-  imgKeyDown.textContent = 'I am a boat in water';
+//keydown event (not sure how to get this working)//use on navigation
+//keydown must be on 'document'
+const imgKeyDown = document.querySelector('.content-section .img-content img');
+document.addEventListener('keydown', event => {
+  imgKeyDown.style.border = '5px solid blue';
+  console.log(event);
 });
 
-//wheel event
-const wheelEvent = document.querySelector(
-  '.content-section .text-content .img-content .img'
-);
-wheelEvent.addEventListener('wheel', () => {
-  wheelEvent.style.tranform = 'scale(2.0)';
-});
+//wheel event -> won't work for me because i have trackpad
+// const wheelEvent = document.querySelector(
+//   '.content-section .text-content .img-content img'
+// );
+// wheelEvent.addEventListener('wheel', () => {
+//   wheelEvent.style.transform = 'scale(2.0)';
+// });
 
 //drag and drop event
 
 //load event
 const loadEvent = document.querySelector('body');
-loadEvent.addEventListener('load', () => {
+window.addEventListener('load', () => {
   alert('Welcome to the Fun Bus Experience');
 });
 
-//focus event
+//focus event //
 const focusEvent = document.querySelector('footer');
 focusEvent.addEventListener('focus', () => {
   focusEvent.style.background = 'purple';
 });
 
 //resize event
-const resizeEvent = document.querySelector('.content-destination .img');
-window.addEventListener('resize', () => {
-  resizeEvent.src =
-    'https://images.unsplash.com/photo-1553260234-9450a4ff098d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
+const resizeEvent = document.querySelector('.content-destination img');
+window.addEventListener('resize', event => {
+  resizeEvent.src = 'img/bram-van-oost-oOCI35aIA3M-unsplash.jpg';
+  console.log(event);
 });
 
 //scroll event
-
-window.addEventListener('scroll', event => {
-  body.style.background = 'hotpink';
+const scrollEvent = document.querySelector('body');
+window.addEventListener('scroll', () => {
+  body.style.background = 'red';
 });
 
 //select event
 
 //dblclick event
-const dblClickEvent = document.querySelector(
-  '.main-navigation .container .nav-container .logo-heading'
-);
+
+const dblClickEvent = document.querySelector('.nav a');
 dblClickEvent.addEventListener('dblclick', () => {
   dblClickEvent.textContent = "Todd's Bus";
 });
